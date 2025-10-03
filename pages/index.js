c6f396f65e16f5c112a18e5ba1447b522637b9e8
Brenda <83522147+brendamarialua13@users.noreply.github.com>
2025-10-03 09:45:22 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home () {
+    return <p>You're amazing!
+You just got a milkshake for opening my first code, which is even running on a global server. May it be prosperous!
+I love you.</p>
+}
+export default Home;
